7c354c1a69e74fb36182a1c17ce9c4285c38b4d2
Pinto Manuel <pinto@fireworkx.co.za>
2022-12-12 04:39:08 +0200

    commit code

--- src/compoents/Monolith_Hero_banner.jsx
+++ src/compoents/Monolith_Hero_banner.jsx
@@ -22,11 +22,11 @@ const Monolith_Hero_banner = () => {
         >
             <div className='container mx-auto  sm:max-w-7xl  text-start'>
                 <h3 className='text-tinyText m-5 sm:m-0 font-bold text-xl mb-2 mt-20 sm:mt-0'>Monolith</h3>
-                <h2 className='text-5xl font-semibold text-white mb-4 sm:w-full w-[400px] m-5 sm:m-0 leading-[1.2]'>Test less. Learn more.</h2>
+                <h2 className='text-5xl font-semibold text-white mb-4 sm:w-full w-[400px] m-5  sm:m-0 leading-[1.2]'>Test less. Learn more.</h2>
                 <p className='text-white text-lg font-semibold  sm:w-[750px] leading-[1.9] m-5 sm:m-0 sm:mb-20'>Spend less time running expensive, repetitive tests and more time learning from your engineering data to predict the exact tests to run.</p>
 
                 {/*button*/}
-                <div className='container mx-auto cursor-pointer flex flex-col sm:flex-row gap-4 w-full mb-28'>
+                <div className='container mx-auto cursor-pointer flex flex-col sm:flex-row gap-4 w-full mb-28 sm:mb-40'>
                     <div className='mb-10 m-5 sm:m-0'>
                         <a onClick={() => setmyModel(true)} className='text-white font-bold bg-tinyText p-5 pl-14 pr-14 rounded-[50px] sm:w-auto w-full hover:opacity-90 hover:bg-btnBG ' href='#'>REQUEST DEMO</a>
                     </div>
